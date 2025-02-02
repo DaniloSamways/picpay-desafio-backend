1997d14a9f81f36d8537c25c89854edac497d2da
Danilo Samways <hadeflowing@gmail.com>
2025-02-02 20:36:05 -0300

    feat: add create transaction validation

---
+++ src/main/java/br/com/danilosamways/exception/InvalidTransactionException.java
@@ -0,0 +1,7 @@
+package br.com.danilosamways.exception;
+
+public class InvalidTransactionException extends RuntimeException {
+    public InvalidTransactionException(String message) {
+        super(message);
+    }
+}
--- src/main/java/br/com/danilosamways/picpay_desafio_backend/transaction/TransactionService.java
+++ src/main/java/br/com/danilosamways/picpay_desafio_backend/transaction/TransactionService.java
@@ -1,8 +1,12 @@
 package br.com.danilosamways.picpay_desafio_backend.transaction;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.danilosamways.exception.InvalidTransactionException;
+import br.com.danilosamways.picpay_desafio_backend.wallet.Wallet;
 import br.com.danilosamways.picpay_desafio_backend.wallet.WalletRepository;
+import br.com.danilosamways.picpay_desafio_backend.wallet.WalletType;
 
 @Service
 public class TransactionService {
@@ -14,8 +18,10 @@ public class TransactionService {
         this.walletRepository = walletRepository;
     }
 
+    @Transactional
     public Transaction create(Transaction transaction) {
         // 1 - Validação
+        validate(transaction);
 
         // 2 - Criar a carteira
         var newTransaction = transactionRepository.save(transaction);
@@ -29,5 +35,27 @@ public class TransactionService {
         // 4 - Chamar serviços externos
 
         return newTransaction;
+    }
+
+    /*
+     * - the payer has a common wallet
+     * - the payer has enough balance
+     * - the payer is not the payee
+     */
+    private void validate(Transaction transaction) {
+        walletRepository.findById(transaction.payee())
+                .map(payee -> walletRepository.findById(transaction.payer())
+                        .map(payer -> isTransactionValid(transaction, payer) ? transaction : null)
+                        .orElseThrow(
+                                () -> new InvalidTransactionException(
+                                        "Invalid transaction - %s".formatted(transaction))))
+                .orElseThrow(() -> new InvalidTransactionException(
+                        "Invalid transaction - %s".formatted(transaction)));
+    }
+
+    private boolean isTransactionValid(Transaction transaction, Wallet payer) {
+        return payer.type() == WalletType.COMUM.getValue() &&
+                payer.balance().compareTo(transaction.value()) >= 0 &&
+                !transaction.payer().equals(transaction.payee());
     }
 }
